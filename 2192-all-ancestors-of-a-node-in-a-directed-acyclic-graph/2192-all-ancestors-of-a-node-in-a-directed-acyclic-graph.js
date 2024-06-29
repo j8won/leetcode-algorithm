10df30a13b044c9db5ba98dccad91e2978c91019
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2024-06-30 03:38:36 +0900

    Time: 957 ms (5.66%), Space: 90.6 MB (13.21%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.js
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.js
@@ -4,17 +4,40 @@
  * @return {number[][]}
  */
 var getAncestors = function(n, edges) {
-    // let graph = Array.from({length:n}, () => []);
+    let graph = Array.from({length:n}, () => new Set())
+
+    for (edge of edges){
+        let [f, t] = edge;
+        graph[f].add(t);
+    }
+
+    function dfs(graph, parent, cur, result, visit) {
+        visit[cur] = true;
+        for (let node of graph[cur]) {
+            if (!visit[node]) {
+                result[node].add(parent);
+                dfs(graph, parent, node, result, visit)
+            }
+        }
+    }
+
+    let result = Array.from({length:n}, () => new Set());
+    for (let i=0; i<n; i++){
+        dfs(graph, i, i, result, Array.from({length:n}, () => false))
+    }
+
+    return result.map(x => [...x])
+   
+};
+
+
+var getAncestors2 = function(n, edges) {
     let reversedG = Array.from({length:n}, () => new Set())
 
     for (edge of edges){
         let [f, t] = edge;
-        // graph[f].push(t);
         reversedG[t].add(f);
     }
-
-    // console.log(graph);
-    // console.log(reversedG);
 
     for (let parents of reversedG){
         for (let node of parents){
