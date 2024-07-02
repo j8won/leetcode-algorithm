f6d5a0b27ee42a115623e2e0e5e95b2b675b125f
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2024-07-02 20:16:31 +0900

    Time: 48 ms (92.79%), Space: 51.3 MB (34.38%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -4,42 +4,24 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    let map1 = {}
-    let map2 = {}
+    let map = {}
+    let answer = [];
 
     for (x of nums1) {
-        if (map1[x]) {
-            map1[x]++;
+        if (map[x]) {
+            map[x]++;
         } else {
-            map1[x] = 1;
+            map[x] = 1;
         }
     }
 
     for (x of nums2) {
-        if (map2[x]) {
-            map2[x]++
-        } else {
-            map2[x] = 1;
+        if (map[x]) {
+            map[x]--;
+            answer.push(x);
         }
     }
 
-    let [smallMap, bigMap] = [];
-
-    if (Object.keys(map1).length > Object.keys(map2).length) {
-        [smallMap, bigMap] = [map2, map1];
-    } else {
-        [smallMap, bigMap] = [map1, map2];
-    }
-
-    console.log(smallMap, bigMap);
-    let answer = [];
-
-    for (let key of Object.keys(smallMap)) {
-        if (bigMap[key] >= 1) {
-            let cnt = Math.min(smallMap[key], bigMap[key]);
-            Array.from({length: cnt}).forEach(() => answer.push(key))
-        }
-    }
 
     return answer;
 };
