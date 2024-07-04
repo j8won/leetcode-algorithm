4c3430a88cf965099dd8e9d2fda0455f9ed97b28
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2024-07-04 16:31:49 +0900

    Time: 70 ms (30.75%), Space: 53.9 MB (65.4%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let start = 0;
+    let end = 0;
+
+    while (end < nums.length) {
+        if (nums[end] === 0) k--;
+        if (k < 0) {
+            if (nums[start] === 0) k++;
+            start++;
+        }
+        end++;
+    }
+
+    return end-start
+};
