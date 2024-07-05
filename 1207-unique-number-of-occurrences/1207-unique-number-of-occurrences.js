911f60f69702e26220e50df80b655614e4c19a6a
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2024-07-05 15:59:09 +0900

    Time: 65 ms (15.63%), Space: 50.3 MB (48.67%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    let map = {};
+
+    for (let x of arr) {
+        map[x] ? map[x]++ : map[x] = 1;
+    }
+
+    let values = Object.values(map);
+    let set = new Set(values);
+
+    return values.length === [...set].length;
+};
