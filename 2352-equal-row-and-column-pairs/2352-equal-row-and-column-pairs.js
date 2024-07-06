0872bb8c6d5104489d374af6fdf5c2173d3fc4d0
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2024-07-06 21:19:24 +0900

    Time: 100 ms (68.71%), Space: 60.6 MB (30.17%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
@@ -4,27 +4,19 @@
  */
 var equalPairs = function (grid) {
     let columns = {};
-    let rows = {};
 
     for (let x of grid) {
         x = x.join(",");
         columns[x] = (columns[x] || 0) + 1;
     }
 
+    let answer = 0;
     for (let i = 0; i < grid.length; i++) {
         let x = [];
         grid.forEach((a) => x.push(a[i]));
-        console.log(x)
         x.join(",");
-        rows[x] = (rows[x] || 0) + 1;
+        if (columns[x]) answer += columns[x];
     }
-
-    let answer = 0;
-    for (let x of Object.keys(columns)){
-        if (rows[x]) {
-            answer += columns[x] * rows[x];
-        }
-    }
-
+    
     return answer;
 };
