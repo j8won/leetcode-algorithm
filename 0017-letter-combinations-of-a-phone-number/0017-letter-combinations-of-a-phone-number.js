a50723e73d8fba02fcdd7c0919776daf8d3be585
Joowon Kyung <75734220+j8won@users.noreply.github.com>
2024-09-04 14:44:04 +0900

    Time: 52 ms (52.33%), Space: 48.7 MB (81.88%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+const keyboard = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+}
+
+var letterCombinations = function (digits) {
+    digits = digits.split("");
+    if (digits.length === 0) return [];
+
+    const result = [];
+
+    const backtrack = (index, path) => {
+        if (index === digits.length) {
+            result.push(path);
+            return;
+        }
+
+        const cur = digits[index];
+        const letters = keyboard[cur]
+
+        for (const letter of letters){
+            backtrack(index+1, path+letter)
+        }
+    }
+
+    backtrack(0, "");
+    return result;
+};
